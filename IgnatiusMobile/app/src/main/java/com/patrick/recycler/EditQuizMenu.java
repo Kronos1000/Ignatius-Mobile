47b3fa69f835c6e296b0d680800d841a5cfd1fa7
Patrick Ware <101075639+Kronos1000@users.noreply.github.com>
2023-06-13 22:43:29 +1200

    updated editquiz menu
    updated edit quiz menu to include an alert dialog to inform users how to dd and remove questions from the quiz bank

--- IgnatiusMobile/app/src/main/java/com/patrick/recycler/EditQuizMenu.java
+++ IgnatiusMobile/app/src/main/java/com/patrick/recycler/EditQuizMenu.java
@@ -1,7 +1,8 @@
 package com.patrick.recycler;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +10,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.patrick.recycler.DBHelper;
+
 public class EditQuizMenu extends AppCompatActivity {
-    EditText question,subject,option1,option2,option3,answer;
-    Button insert,view,delete;
+    EditText question, subject, option1, option2, option3, answer;
+    Button insert, view, delete;
     DBHelper DB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +29,6 @@ public class EditQuizMenu extends AppCompatActivity {
         option3 = findViewById(R.id.option3);
         answer = findViewById(R.id.answer);
 
-
         insert = findViewById(R.id.btnInsert);
         view = findViewById(R.id.btnView);
         delete = findViewById(R.id.btnDelete);
@@ -35,13 +38,11 @@ public class EditQuizMenu extends AppCompatActivity {
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(EditQuizMenu.this,Questionlist.class ));
+                startActivity(new Intent(EditQuizMenu.this, Questionlist.class));
             }
         });
 
-
         // insert data method
-
         insert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,26 +53,19 @@ public class EditQuizMenu extends AppCompatActivity {
                 String option3TXT = option3.getText().toString();
                 String answerTXT = answer.getText().toString();
 
-                Boolean checkinsertdata = DB.insertquizdata(questionTXT,subjectTXT,option1TXT,option2TXT,option3TXT,answerTXT);
-                if(checkinsertdata == true)
-                {
+                boolean checkinsertdata = DB.insertquizdata(questionTXT, subjectTXT, option1TXT, option2TXT, option3TXT, answerTXT);
+                if (checkinsertdata) {
                     Toast.makeText(EditQuizMenu.this, "New Question Added", Toast.LENGTH_SHORT).show();
-                }
-
-                else
-                {
+                } else {
                     Toast.makeText(EditQuizMenu.this, "No changes have been made", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 String questionTXT = question.getText().toString();
-
                 boolean checkDeleteData = DB.deleteQuizData(questionTXT);
                 if (checkDeleteData) {
                     Toast.makeText(EditQuizMenu.this, "Question Deleted", Toast.LENGTH_SHORT).show();
@@ -80,5 +74,24 @@ public class EditQuizMenu extends AppCompatActivity {
                 }
             }
         });
+
+        // Show an alert dialog when the activity is created
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Instructions");
+        builder.setMessage("How to Add new Question \n" +
+                "Fill in the form with all the required information.\n\n" +
+                "How To delete a Question \n" +
+                "To Delete a question from the quiz, simply write the question you wish to remove and press the delete button.No Need to" +
+                "fill out the entire form.");
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // Handle the "OK" button click if needed
+            }
+        });
+        builder.setCancelable(false); // Prevent dialog from being dismissed when pressing outside of it
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
