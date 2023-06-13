8c901ed025d0daf5d56c634b4cbc783c5bd9032d
Patrick Ware <101075639+Kronos1000@users.noreply.github.com>
2023-06-13 14:56:00 +1200

    redo

---
+++ StableApplication Backups/IgnatiusMobile v1.02/IgnatiusMobile/app/src/main/java/com/patrick/recycler/DBHelper.java
@@ -0,0 +1,60 @@
+package com.patrick.recycler;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public DBHelper(Context context) {
+        super(context, "Quizdata.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase DB) {
+        DB.execSQL("create Table Questions (question TEXT primary key, subject TEXT,option1 TEXT,option2 TEXT,option3 TEXT,answer TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB, int oldVersion, int newVersion) {
+        DB.execSQL("drop Table if exists Questions");
+    }
+
+
+    public Boolean insertquizdata(String question, String subject, String option1, String option2, String option3, String answer) {
+
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("question", question);
+        contentValues.put("subject", subject);
+        contentValues.put("option1", option1);
+        contentValues.put("option2", option2);
+        contentValues.put("option3", option3);
+        contentValues.put("answer", answer);
+
+        long result = DB.insert("Questions", null, contentValues);
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean deleteQuizData(String question) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String[] whereArgs = { question };
+        int result = db.delete("Questions", "question=?", whereArgs);
+        return result > 0;
+    }
+    public Cursor getdata()
+    {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select * From Questions ",null );
+        return  cursor;
+    }
+
+}
