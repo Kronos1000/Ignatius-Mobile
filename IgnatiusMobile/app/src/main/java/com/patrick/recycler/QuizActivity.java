682bf39460db89251c2d21d33b4990c12e595e90
Patrick Ware <101075639+Kronos1000@users.noreply.github.com>
2023-06-18 23:46:33 +1200

    Quiz  almost Working

--- IgnatiusMobile/app/src/main/java/com/patrick/recycler/QuizActivity.java
+++ IgnatiusMobile/app/src/main/java/com/patrick/recycler/QuizActivity.java
@@ -1,27 +1,121 @@
 package com.patrick.recycler;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class QuizActivity extends AppCompatActivity {
 
-    private TextView questionText;
-    private RadioGroup optionsGroup;
-    private SQLiteDatabase database;
+    TextView questionText;
+    Button button_option1;
+    Button button_option2;
+    Button button_option3;
+
+    SQLiteDatabase database;
+    DBHelper DB;
+
+    ArrayList<String> question;
+    ArrayList<String> option1;
+    ArrayList<String> option2;
+    ArrayList<String> option3;
+    ArrayList<String> answer;
+    int currentQuestionIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        DB = new DBHelper(this);
 
+        // Retrieve quiz data from intent extras
+        String selectedSubject = getIntent().getStringExtra("selectedSubject");
 
+        questionText = findViewById(R.id.QuestionText);
+        button_option1 = findViewById(R.id.button_option1);
+        button_option2 = findViewById(R.id.button_option2);
+        button_option3 = findViewById(R.id.button_option3);
+
+        question = new ArrayList<>();
+        option1 = new ArrayList<>();
+        option2 = new ArrayList<>();
+        option3 = new ArrayList<>();
+        answer = new ArrayList<>();
+        currentQuestionIndex = 0;
+
+        loadQuizData(selectedSubject);
+
+        displayQuestion();
+
+        button_option1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(button_option1.getText().toString());
+            }
+        });
+
+        button_option2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(button_option2.getText().toString());
+            }
+        });
+
+        button_option3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(button_option3.getText().toString());
+            }
+        });
+    }
+
+    private void loadQuizData(String selectedSubject) {
+        Cursor cursor = DB.getQuizData(selectedSubject);
+        while (cursor.moveToNext()) {
+            question.add(cursor.getString(0));
+            option1.add(cursor.getString(2));
+            option2.add(cursor.getString(3));
+            option3.add(cursor.getString(4));
+            answer.add(cursor.getString(5));
+        }
+    }
+
+    private void displayQuestion() {
+        if (currentQuestionIndex < question.size()) {
+            questionText.setText(question.get(currentQuestionIndex));
+            button_option1.setText(option1.get(currentQuestionIndex));
+            button_option2.setText(option2.get(currentQuestionIndex));
+            button_option3.setText(option3.get(currentQuestionIndex));
+        } else {
+            // Quiz finished, display a message or perform necessary actions
+
+        }
+    }
+
+    private void checkAnswer(String selectedOption) {
+        int rightAnswers = 0;
+        int questionCount = 0;
+        String correctAnswer = answer.get(currentQuestionIndex);
+        if (selectedOption.equals(correctAnswer)) {
+            // Correct answer selected
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
+            rightAnswers ++;
+            questionCount++;
+        } else {
+            // Incorrect answer selected
+            Toast.makeText(this, "Incorrect!", Toast.LENGTH_SHORT).show();
+        }
+
+        // Move to the next question
+        currentQuestionIndex++;
+        displayQuestion();
     }
 }
--- IgnatiusMobile/app/src/main/java/com/patrick/recycler/Quizme.java
+++ IgnatiusMobile/app/src/main/java/com/patrick/recycler/Quizme.java
@@ -2,8 +2,11 @@ package com.patrick.recycler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -12,7 +15,8 @@ import java.util.ArrayList;
 
 public class Quizme extends AppCompatActivity {
     ListView listView;
-    ArrayList<String> subject;
+    ArrayList<String> subjects;
+
     DBHelper DB;
     ArrayAdapter<String> adapter;
 
@@ -21,19 +25,34 @@ public class Quizme extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizme);
         DB = new DBHelper(this);
-        subject = new ArrayList<>();
+
+        subjects = new ArrayList<>();
 
         listView = findViewById(R.id.topic_list);
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, subject);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, subjects);
         listView.setAdapter(adapter);
         displayData();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedSubject = subjects.get(position);
+                startQuiz(selectedSubject);
+            }
+        });
     }
 
     private void displayData() {
         Cursor cursor = DB.getTopics();
         while (cursor.moveToNext()) {
-            subject.add(cursor.getString(0));
+            subjects.add(cursor.getString(0));
         }
         adapter.notifyDataSetChanged();
+    }
+
+    private void startQuiz(String selectedSubject) {
+        Intent intent = new Intent(Quizme.this, QuizActivity.class);
+        intent.putExtra("selectedSubject", selectedSubject);
+        startActivity(intent);
     }
 }
