ffeefd24a622920ba0304dc3ff7fc22c279c095d
Patrick Ware <101075639+Kronos1000@users.noreply.github.com>
2023-06-27 18:31:01 +1200

    Submission Commit

---
+++ 2021001676_IT617_Assignment1/IgnatiusMobile/app/src/main/java/com/patrick/recycler/MainActivity.java
@@ -0,0 +1,56 @@
+package com.patrick.recycler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        AdapterView.OnItemClickListener itemClickListener = new
+                AdapterView.OnItemClickListener() {
+                    @Override
+                    // Main Menu setup
+                    public void onItemClick(AdapterView<?> adapterView, View view,
+                                            int position, long id) {
+                        // Start Quiz menu
+                        if (position == 0) {
+                            Intent intent = new Intent(MainActivity.this,
+                                   Quizme.class);
+                            startActivity(intent);
+                        }
+
+
+                        // Go to quiz menu option
+                        if (position == 1) {
+                            Intent intent = new Intent(MainActivity.this,
+                                   EditQuizMenu.class);
+                            startActivity(intent);
+                        }
+
+
+
+
+                        // Exit Application
+
+                        if (position == 2) {
+                            finish();
+                            System.exit(0);
+                        }
+                    }
+                };
+        ListView listView = findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
+    }
+
+
+}
---
+++ 2021001676_IT617_Assignment1/IgnatiusMobile/app/src/main/java/com/patrick/recycler/QuizActivity.java
@@ -0,0 +1,181 @@
+package com.patrick.recycler;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.snackbar.Snackbar;
+import java.util.ArrayList;
+
+public class QuizActivity extends AppCompatActivity {
+
+    TextView questionText;
+    Button button_option1;
+    Button button_option2;
+    Button button_option3;
+
+    SQLiteDatabase database;
+    DBHelper DB;
+
+    ArrayList<String> question;
+    ArrayList<String> option1;
+    TextView questionCounterText;
+    ArrayList<String> option2;
+    ArrayList<String> option3;
+    ArrayList<String> answer;
+    int currentQuestionIndex;
+    int rightAnswers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        DB = new DBHelper(this);
+
+        // Retrieve quiz data from intent extras
+        String selectedSubject = getIntent().getStringExtra("selectedSubject");
+
+        questionText = findViewById(R.id.QuestionText);
+        questionCounterText = findViewById(R.id.questionCounterText);
+        button_option1 = findViewById(R.id.button_option1);
+        button_option2 = findViewById(R.id.button_option2);
+        button_option3 = findViewById(R.id.button_option3);
+        TextView questionCounterText;
+        question = new ArrayList<>();
+        option1 = new ArrayList<>();
+        option2 = new ArrayList<>();
+        option3 = new ArrayList<>();
+        answer = new ArrayList<>();
+        currentQuestionIndex = 0;
+        rightAnswers = 0;
+
+        loadQuizData(selectedSubject);
+
+        displayQuestion();
+
+        button_option1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(button_option1.getText().toString());
+            }
+        });
+
+        button_option2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(button_option2.getText().toString());
+            }
+        });
+
+        button_option3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(button_option3.getText().toString());
+            }
+        });
+    }
+
+    private void loadQuizData(String selectedSubject) {
+        Cursor cursor = DB.getQuizData(selectedSubject);
+        while (cursor.moveToNext()) {
+            question.add(cursor.getString(0));
+            option1.add(cursor.getString(2));
+            option2.add(cursor.getString(3));
+            option3.add(cursor.getString(4));
+            answer.add(cursor.getString(5));
+        }
+    }
+
+    private void displayQuestion() {
+        if (currentQuestionIndex < question.size()) {
+            questionText.setText(question.get(currentQuestionIndex));
+            button_option1.setText(option1.get(currentQuestionIndex));
+            button_option2.setText(option2.get(currentQuestionIndex));
+            button_option3.setText(option3.get(currentQuestionIndex));
+
+            String counterText = "Question " + (currentQuestionIndex + 1) + " of " + question.size();
+            questionCounterText.setText(counterText);
+        } else {
+            showResultDialog();
+        }
+    }
+
+    private void checkAnswer(String selectedOption) {
+        String correctAnswer = answer.get(currentQuestionIndex);
+        if (selectedOption.equals(correctAnswer)) {
+            View rootView =  findViewById(android.R.id.content);
+            Snackbar.make(rootView, R.string.correct_answer_snackbar, Snackbar.LENGTH_SHORT).show();
+
+            rightAnswers++;
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.wrong_answer_alert);
+            String questionAlert = question.get(currentQuestionIndex);
+            builder.setMessage("Question:\n"+ questionAlert +"\n\n" + "Your Answer:\n" + selectedOption + "\n\n\nCorrect Answer:\n" + correctAnswer);
+
+            builder.setPositiveButton(R.string.ok_button, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    // Handle the "OK" button click if needed
+                }
+            });
+
+            builder.setCancelable(false);
+            AlertDialog alertDialog = builder.create();
+
+            alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                @Override
+                public void onShow(DialogInterface dialog) {
+                    Button positiveButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+                    positiveButton.setTextColor(Color.parseColor("#12bb08"));
+                }
+            });
+
+            alertDialog.show();
+        }
+
+        currentQuestionIndex++;
+        displayQuestion();
+    }
+
+    private void showResultDialog() {
+        int score = rightAnswers;
+        String message = "Congratulations! You Scored " + score + "/" + question.size();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Quiz Results")
+                .setMessage(message);
+        builder.setPositiveButton(R.string.ok_button, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(QuizActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                })
+                .setCancelable(false)
+                .show();
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                Button positiveButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+                positiveButton.setTextColor(Color.parseColor("#12bb08"));
+            }
+        });
+
+        alertDialog.show();
+    }
+}
