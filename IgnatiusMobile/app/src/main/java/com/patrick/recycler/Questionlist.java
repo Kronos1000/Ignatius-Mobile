07e31609c46b55816f90332b8e8b83cacf7fff5c
Patrick Ware <101075639+Kronos1000@users.noreply.github.com>
2023-08-13 18:55:49 +1200

    update question recyccler view

--- IgnatiusMobile/app/src/main/java/com/patrick/recycler/Questionlist.java
+++ IgnatiusMobile/app/src/main/java/com/patrick/recycler/Questionlist.java
@@ -47,12 +47,12 @@ MyAdapter adapter;
         {
             while(cursor.moveToNext())
             {
-                question.add(cursor.getString(0));
-                subject.add(cursor.getString(1));
-                option1.add(cursor.getString(2));
-                option2.add(cursor.getString(3));
-                option3.add(cursor.getString(4));
-                answer.add(cursor.getString(5));
+                question.add(cursor.getString(1));
+                subject.add(cursor.getString(2));
+                option1.add(cursor.getString(3));
+                option2.add(cursor.getString(4));
+                option3.add(cursor.getString(5));
+                answer.add(cursor.getString(6));
             }
         }
 
