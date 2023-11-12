d1173b8f4c6ac8dc0324082abaf4127a33375778
Patrick Ware <101075639+Kronos1000@users.noreply.github.com>
2023-11-12 19:31:46 +1300

    updated end of quiz message
    inform users what questions they got wrong

--- IgnatiusMobile/app/src/main/java/com/patrick/recycler/QuizActivity.java
+++ IgnatiusMobile/app/src/main/java/com/patrick/recycler/QuizActivity.java
@@ -1,8 +1,5 @@
 package com.patrick.recycler;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -12,10 +9,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.snackbar.Snackbar;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -29,15 +30,16 @@ public class QuizActivity extends AppCompatActivity {
 
     ArrayList<String> question;
     ArrayList<String> option1;
-
-  TextView incorrectAnswerText;
-    TextView questionCounterText;
     ArrayList<String> option2;
     ArrayList<String> option3;
     ArrayList<String> answer;
+
+    TextView incorrectAnswerText;
+    TextView questionCounterText;
     int currentQuestionIndex;
     int rightAnswers;
     int wrongAnswers;
+    List<Integer> incorrectQuestionIndexes;  // List to store indexes of incorrect questions
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,18 +57,19 @@ public class QuizActivity extends AppCompatActivity {
         button_option1 = findViewById(R.id.button_option1);
         button_option2 = findViewById(R.id.button_option2);
         button_option3 = findViewById(R.id.button_option3);
-        TextView questionCounterText;
+
         question = new ArrayList<>();
         option1 = new ArrayList<>();
         option2 = new ArrayList<>();
         option3 = new ArrayList<>();
         answer = new ArrayList<>();
+
         currentQuestionIndex = 0;
         rightAnswers = 0;
         wrongAnswers = 0;
+        incorrectQuestionIndexes = new ArrayList<>();
 
         loadQuizData(selectedSubject);
-
         displayQuestion();
 
         button_option1.setOnClickListener(new View.OnClickListener() {
@@ -95,7 +98,6 @@ public class QuizActivity extends AppCompatActivity {
         Cursor cursor = DB.getQuizData(selectedSubject);
         while (cursor.moveToNext()) {
             question.add(cursor.getString(1));
-
             option1.add(cursor.getString(3));
             option2.add(cursor.getString(4));
             option3.add(cursor.getString(5));
@@ -106,15 +108,13 @@ public class QuizActivity extends AppCompatActivity {
     private void displayQuestion() {
         if (currentQuestionIndex < question.size()) {
             questionText.setText(question.get(currentQuestionIndex));
-            incorrectAnswerText.setText("Incorrect Answers: "+ wrongAnswers);
+            incorrectAnswerText.setText("Incorrect Answers: " + wrongAnswers);
             button_option1.setText(option1.get(currentQuestionIndex));
             button_option2.setText(option2.get(currentQuestionIndex));
             button_option3.setText(option3.get(currentQuestionIndex));
 
             String counterText = "Question " + (currentQuestionIndex + 1) + " of " + question.size();
-
             questionCounterText.setText(counterText);
-
         } else {
             showResultDialog();
         }
@@ -123,45 +123,58 @@ public class QuizActivity extends AppCompatActivity {
     private void checkAnswer(String selectedOption) {
         String correctAnswer = answer.get(currentQuestionIndex);
         if (selectedOption.equals(correctAnswer)) {
-            View rootView =  findViewById(android.R.id.content);
+            View rootView = findViewById(android.R.id.content);
             Snackbar.make(rootView, R.string.correct_answer_snackbar, Snackbar.LENGTH_SHORT).show();
-
             rightAnswers++;
         } else {
             wrongAnswers++;
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle(R.string.wrong_answer_alert);
-            String questionAlert = question.get(currentQuestionIndex);
-            builder.setMessage("Question:\n"+ questionAlert +"\n\n" + "Your Answer:\n" + selectedOption + "\n\n\nCorrect Answer:\n" + correctAnswer);
-
-            builder.setPositiveButton(R.string.ok_button, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    // Handle the "OK" button click if needed
-                }
-            });
-
-            builder.setCancelable(false);
-            AlertDialog alertDialog = builder.create();
-
-            alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
-                @Override
-                public void onShow(DialogInterface dialog) {
-                    Button positiveButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
-                    positiveButton.setTextColor(Color.parseColor("#12bb08"));
-                }
-            });
-
-            alertDialog.show();
+            incorrectQuestionIndexes.add(currentQuestionIndex);  // Add index of incorrect question
+            showWrongAnswerDialog(question.get(currentQuestionIndex), selectedOption, correctAnswer);
         }
 
         currentQuestionIndex++;
         displayQuestion();
     }
 
+    private void showWrongAnswerDialog(String questionText, String selectedOption, String correctAnswer) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.wrong_answer_alert);
+        builder.setMessage("Question:\n" + questionText + "\n\n" +
+                "Your Answer:\n" + selectedOption + "\n\n\n" +
+                "Correct Answer:\n" + correctAnswer);
+
+        builder.setPositiveButton(R.string.ok_button, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // Handle the "OK" button click if needed
+            }
+        });
+
+        builder.setCancelable(false);
+        AlertDialog alertDialog = builder.create();
+
+        alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                Button positiveButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+                positiveButton.setTextColor(Color.parseColor("#12bb08"));
+            }
+        });
+
+        alertDialog.show();
+    }
+
     private void showResultDialog() {
         int score = rightAnswers;
         String message = "Congratulations! You Scored " + score + "/" + question.size();
+
+        if (!incorrectQuestionIndexes.isEmpty()) {
+            StringBuilder incorrectQuestionsMessage = new StringBuilder("\n\n--- What did you get wrong? ---\n\n");
+            for (int incorrectIndex : incorrectQuestionIndexes) {
+                incorrectQuestionsMessage.append("").append(question.get(incorrectIndex)).append("\n\n");
+            }
+            message += incorrectQuestionsMessage.toString();
+        }
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Quiz Results")
@@ -185,7 +198,6 @@ public class QuizActivity extends AppCompatActivity {
                 positiveButton.setTextColor(Color.parseColor("#12bb08"));
             }
         });
-
         alertDialog.show();
     }
 }
